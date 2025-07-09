ac56d4f1cc93c9c5bf5a3891f340c1f90ffb42f0
Fei Peng <fpen0007@student.monash.edu>
2025-07-09 13:14:40 +1000

    - delete all files to change this repo

--- Libraries/com.unity.xr.visionos/Runtime/Plugins/visionos/VisionOSSettings.swift
+++
@@ -1,7 +0,0 @@
-import SwiftUI
-
-var VisionOSEnableFoveation = true
-var VisionOSUpperLimbVisibility: Visibility = .hidden
-var VisionOSImmersionStyle: ImmersionStyle = .automatic
-var VisionOSSkipPresent = true
-var VisionOSEDRHeadroom = 2
